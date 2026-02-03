d3bdfffbe487e92835011253e583836bf0d25422
shrys <shreyasjain2018@gmail.com>
2026-02-03 18:13:15 +0530

    release: finalize icons, widget UI, signing config and production resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,10 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+
+
+
     }
 
     buildTypes {
@@ -27,11 +31,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
